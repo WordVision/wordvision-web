7036ca10a51a6c8b471f16b08a90a20f04c22699
John Pierre Mendoza <johnpierremendoza@gmail.com>
2025-05-17 21:44:45 -0400

    fix: fix text selection on IOS
    just had to remove an overlay

--- app/reader/components/ActionBar.tsx
+++ app/reader/components/ActionBar.tsx
@@ -14,13 +14,6 @@ interface ActionBarProps {
 export default function ActionBar(p: ActionBarProps) {
 
   return <>
-    {p.show &&
-      <div
-        className="fixed inset-0 z-50 bg-transparent"
-        onClick={p.dismissHandler}
-      ></div>
-    }
-
     <div
       className={`fixed bottom-0 left-0 right-0 z-50 flex p-4 items-center transition ${p.show ? "" : "translate-y-full"}`}
     >
