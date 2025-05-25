e67bb737c14c8d45171fa3e382d7101b66938810
John Pierre Mendoza <johnpierremendoza@gmail.com>
2025-05-25 14:02:35 -0400

    feat: create a reusable HeaderLayout component

---
+++ components/HeaderLayout.tsx
@@ -0,0 +1,25 @@
+import { Playfair_Display } from "next/font/google";
+import { ReactNode } from "react";
+
+const playfairDisplay = Playfair_Display({
+  weight: '700',
+  style: "italic",
+  subsets: ['latin'],
+})
+
+interface HeaderProps {
+  text: string;
+  children: ReactNode
+}
+
+export default function HeaderLayout(p: HeaderProps) {
+  return (
+    <div className="p-4 flex justify-between items-center">
+      <p
+        style={playfairDisplay.style}
+        className="text-[42px]">{p.text}
+      </p>
+        {p.children}
+    </div>
+  );
+}
