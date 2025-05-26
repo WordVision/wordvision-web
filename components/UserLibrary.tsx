a227cbb8c8897c1a72358d4a593c7fb19291011f
John Pierre Mendoza <johnpierremendoza@gmail.com>
2025-05-26 15:09:47 -0400

    feat: partially implement library styles

--- components/UserLibrary.tsx
+++ components/UserLibrary.tsx
@@ -4,11 +4,11 @@ import { useCallback, useEffect, useRef, useState } from "react";
 import { createClient } from "@/utils/supabase/client";
 import Link from "next/link";
 import Image from "next/image";
-import { redirect } from "next/navigation";
 import { Inter } from "next/font/google";
 
 import FileUploader, { FileUploaderHandle } from "./FileUploader";
 import HeaderLayout from "@/components/HeaderLayout";
+import Avatar from "./Avatar";
 
 type Book = {
   id: string;
@@ -24,6 +24,7 @@ type Props = {
   };
 };
 
+const inter600 = Inter({ weight: "600", subsets: ["latin"] });
 const inter500 = Inter({ weight: "500", subsets: ["latin"] });
 
 export default function UserLibrary({ user }: Props) {
@@ -96,79 +97,87 @@ export default function UserLibrary({ user }: Props) {
     return <div className="text-center py-8 text-red-500">{error}</div>;
 
   return (
-    <div className="p-4">
+    <div className="flex flex-col h-screen">
+
       <HeaderLayout text="Discover">
         <Link href="/profile">
-          <button className="p-3 rounded-full active:bg-neutral-50">
-            <div className="pt-1 pb-2 px-2 flex flex-col items-center gap-2 border-b">
-              <div
-                style={inter500.style}
-                className="w-[72px] h-[72px] text-2xl flex justify-center items-center rounded-full shadow-[inset_0px_-8px_16px_0px_rgba(0,0,0,0.1)]"
-              >
-                {userData.firstName[0] + userData.lastName[0]}
-              </div>
-            </div>
+          <button className="rounded-full active:bg-neutral-50">
+            <Avatar
+              firstName={userData?.firstName!}
+              lastName={userData?.lastName!}
+              width={40}
+              height={40}
+              fontSize={16}
+            />
           </button>
         </Link>
       </HeaderLayout>
-      <h2 className="text-2xl font-bold text-gray-800 mb-4">Featured</h2>
 
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3">
-        {books.map((book) => (
-          <Link key={book.id} href={`/reader/${book.id}`}>
-            <div className="cursor-pointer">
-              {book.cover_url ? (
-                <Image
-                  src={book.cover_url}
-                  alt={book.title}
-                  width={300}
-                  height={180}
-                  className="w-full h-[180px] object-cover rounded-lg"
-                />
-              ) : (
-                <div className="w-full h-[180px] bg-gray-100 flex items-center justify-center text-gray-500 text-sm rounded-lg">
-                  No Cover
-                </div>
-              )}
-              <h3 className="mt-1 text-[15px] font-semibold text-gray-900 leading-snug break-words">
-                {book.title}
-              </h3>
-              <p className="text-sm text-gray-600 truncate">{book.author}</p>
-            </div>
-          </Link>
-        ))}
+      <div className="py-2 flex flex-col flex-1 gap-2">
+        <h2
+          style={inter600.style}
+          className="px-4 text-2xl font-bold text-gray-800"
+        >
+          Featured
+        </h2>
+
+        <div className="px-[10px] grid grid-cols-2 flex-1 overflow-scroll sm:grid-cols-3 md:grid-cols-4">
+          {books.map((book) => (
+            <Link
+              key={book.id}
+              href={`/reader/${book.id}`}
+              className="px-2 py-1 active:bg-neutral-100 rounded-lg"
+            >
+              <button className="w-full cursor-pointer rounded-lg">
+                {book.cover_url ? (
+                  <Image
+                    src={book.cover_url}
+                    alt={book.title}
+                    width={300}
+                    height={180}
+                    className="w-full h-[180px] object-cover rounded-lg"
+                  />
+                ) : (
+                  <div className="w-full h-[240px] bg-neutral-300 flex items-center justify-center text-gray-500 text-sm rounded-lg">
+                    No Cover
+                  </div>
+                )}
+                <h3
+                  style={inter600.style}
+                  className="mt-[10px] text-[14px] font-semibold text-[#2C3131] leading-snug break-words text-left"
+                >
+                  {book.title}
+                </h3>
+                <p
+                  style={inter500.style}
+                  className="text-[12px] text-[#747878] text-left truncate"
+                >
+                  {book.author}
+                </p>
+              </button>
+            </Link>
+          ))}
+        </div>
+
+        {/* Floating + Button */}
+        <button
+          className="fixed bottom-6 right-6 bg-blue-600 text-white rounded-full w-14 h-14 shadow-lg hover:bg-blue-700 text-3xl flex items-center justify-center"
+          aria-label="Add Book"
+          onClick={() => uploaderRef.current?.openFilePicker()}
+        >
+          +
+        </button>
+
+        {/* Hidden uploader with callback */}
+        <FileUploader
+          ref={uploaderRef}
+          user={user}
+          onUploadComplete={() => {
+            setLoading(true); // Optional: show loading UI
+            fetchUserBooks();
+          }}
+        />
       </div>
-
-      {/* Floating + Button */}
-      <button
-        className="fixed bottom-6 right-6 bg-blue-600 text-white rounded-full w-14 h-14 shadow-lg hover:bg-blue-700 text-3xl flex items-center justify-center"
-        aria-label="Add Book"
-        onClick={() => uploaderRef.current?.openFilePicker()}
-      >
-        +
-      </button>
-
-      {/* Hidden uploader with callback */}
-      <FileUploader
-        ref={uploaderRef}
-        user={user}
-        onUploadComplete={() => {
-          setLoading(true); // Optional: show loading UI
-          fetchUserBooks();
-        }}
-      />
-
-      <button
-        className="py-2 px-4 fixed bottom-6 left-6 bg-blue-600 text-white rounded-full shadow-lg hover:bg-blue-700 text-xl flex items-center justify-center"
-        aria-label="Sign Out"
-        onClick={async () => {
-          const supabase = createClient();
-          await supabase.auth.signOut();
-          return redirect("/login");
-        }}
-      >
-        sign out
-      </button>
     </div>
   );
 }
